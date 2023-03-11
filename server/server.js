400f2ee165fe033082d98242207a1791ea47f132
Madhuri Palanivelu <palsmadhu18@gmail.com>
2023-03-10 20:29:19 -0700

    changing server listening port for heroku errors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const masterchefRouter = require('./masterchef-router');
 
 const app = express();
 const fileServerMiddleware = express.static('public');
+let server = require('http').Server(app);
 
 app.use(bodyParser.json());  // JSON parser
 app.use(bodyParser.urlencoded({ extended: true })); // url-encoded parser
@@ -13,5 +14,7 @@ app.use(cookieParser());
 app.use('/', fileServerMiddleware);
 app.use('/', masterchefRouter); //all REST API's
 
-app.listen(3000, function () {
-    console.log('App started on port 3000');});
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+    console.log('App started on port' + port);});
